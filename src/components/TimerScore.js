bfc18ab15d3d2000bbecefbcf6a210f42beb816c
shakib1729 <shakibahmed0786@gmail.com>
2021-03-22 23:03:55 +0530

    modified TimerScore

--- src/components/TimerScore.js
+++ src/components/TimerScore.js
@@ -2,13 +2,29 @@ import React from 'react';
 import { useGlobalContext } from '../context/globalContext';
 
 const TimerScore = () => {
-  const { timer } = useGlobalContext();
+  const { timer, scores, poses, currPose } = useGlobalContext();
 
   return (
     <section id='sidebar-right'>
       <div id='sidebar-timer'>Timer: {timer}</div>
-      <div id='sidebar-score'>Score</div>
-      <div id='sidebar-poseimg'>Pose Img</div>
+      <div id='sidebar-score'>
+        Score:{' '}
+        {scores.map((score, idx) => {
+          return (
+            <li key={idx}>
+              Pose {idx}: {score}
+            </li>
+          );
+        })}
+      </div>
+      <div id='sidebar-poseimg'>
+        <h3>{poses[currPose].name}</h3>
+        <img
+          className='big-pose-img'
+          src={poses[currPose].img}
+          alt={poses[currPose].name}
+        />
+      </div>
     </section>
   );
 };
