752ecafc23007c7222f3b26ddd22a4b3f36b08cc
shakib1729 <shakibahmed0786@gmail.com>
2021-03-22 23:09:58 +0530

    removed unnecessary text

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,16 +5,6 @@ const Main = () => {
   return (
     <main>
       <Detect />
-      <br /> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio
-      dolores sit laboriosam ex. Vel aperiam minima ullam illum repellendus,
-      laudantium beatae unde, eius ex cupiditate aut corporis minus! Ullam,
-      placeat! Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-      Distinctio est ex, aperiam dicta maxime quo odit tenetur ullam sapiente
-      fugiat necessitatibus minima id vitae consequuntur dolor? Quos eligendi
-      atque voluptas? Lorem ipsum dolor sit amet consectetur adipisicing elit.
-      Veniam consequatur, sequi commodi culpa quibusdam magnam assumenda
-      laudantium vero est quos laborum ex nesciunt libero expedita fugiat. Odit
-      quae sunt reprehenderit?
     </main>
   );
 };
