f7a1e86e74f9677d073c4f1bdc3849a11f395969
shakib1729 <shakibahmed0786@gmail.com>
2021-03-23 23:38:56 +0530

    added toggle for dark mode

--- src/components/DarkMode.js
+++ src/components/DarkMode.js
@@ -15,9 +15,27 @@ const DarkMode = () => {
   }, [theme]);
 
   return (
-    <button className='btn' onClick={toggleTheme}>
-      toggle
-    </button>
+    <div className='toggle-container'>
+      <span style={{ color: theme == 'dark-theme' ? 'grey' : 'yellow' }}>
+        ☀︎
+      </span>
+      <div className='switch-checkbox'>
+        <label className='switch'>
+          <input
+            type='checkbox'
+            onChange={toggleTheme}
+            checked={theme == 'dark-theme'}
+          />
+          <span className='slider round'> </span>
+        </label>
+      </div>
+      <span style={{ color: theme == 'dark-theme' ? '#c96dfd' : 'grey' }}>
+        ☽
+      </span>
+    </div>
+    // <button className='btn' onClick={toggleTheme}>
+    //   toggle
+    // </button>
   );
 };
 
