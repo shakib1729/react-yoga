27ddea23a0a81f8b8aa46304a95176ec84a7f557
shakib1729 <shakibahmed0786@gmail.com>
2021-03-25 12:01:11 +0530

    added main text

--- src/components/MainText.js
+++ src/components/MainText.js
@@ -1,7 +1,27 @@
 import React from 'react';
 
 const MainText = () => {
-  return <div className='neu-card'>Turn On</div>;
+  return (
+    <p className='neu-card main-text'>
+      <ul>
+        <li>1) Select a Pose (Asana) from the list on left side</li>
+        <li>2) Click 'Let's Begin' button below</li>
+        <li>
+          3) Adjust your position within 10 seconds. (The timer on right runs
+          for 10 seconds)
+        </li>
+        <li>
+          4) Keep your face towrads the camera and the camera should have full
+          view including legs
+        </li>
+        <li>
+          5) Perform the selected pose for 10 seconds (The timer again runs for
+          10 seconds)
+        </li>
+        <li>5) The better you perform the pose, more the score you get</li>
+      </ul>
+    </p>
+  );
 };
 
 export default MainText;
