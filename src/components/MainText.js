4d89f988483051bdccb6ce358ec04f6a7085ffd5
shakib1729 <shakibahmed0786@gmail.com>
2021-03-26 19:36:49 +0530

    fixed a typo

--- src/components/MainText.js
+++ src/components/MainText.js
@@ -18,7 +18,7 @@ const MainText = () => {
           5) Perform the selected pose for 10 seconds (The timer again runs for
           10 seconds)
         </li>
-        <li>5) The better you perform the pose, more the score you get</li>
+        <li>6) The better you perform the pose, more the score you get</li>
       </ul>
     </p>
   );
