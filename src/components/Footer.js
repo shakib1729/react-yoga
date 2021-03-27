b98512e493f0662fa21ff4d335bced79ebc6eca6
shakib1729 <shakibahmed0786@gmail.com>
2021-03-27 14:11:59 +0530

    added Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
 const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer>
+      Developed by &nbsp;
+      <a href='https://github.com/shakib1729' target='_blank' rel='noreferrer'>
+        Shakib Ahmed
+      </a>
+    </footer>
+  );
 };
 
 export default Footer;
