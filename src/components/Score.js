c57fd3c72ef39a2e84e3d29f64a8613b5efeb537
shakib1729 <shakibahmed0786@gmail.com>
2021-03-27 15:15:53 +0530

    modified Score

--- src/components/Score.js
+++ src/components/Score.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { useGlobalContext } from '../context/globalContext';
 
 const Score = () => {
-  const { scores } = useGlobalContext();
+  const { scores, poses } = useGlobalContext();
   return (
     <div id='sidebar-score'>
-      Score:{' '}
+      Scores{' '}
       {scores.map((score, idx) => {
         return (
           <li key={idx}>
-            Pose {idx}: {score}
+            {poses[idx].name}: {score} &nbsp; (Best: {score})
           </li>
         );
       })}
